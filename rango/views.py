ac13fec717a941209731eb3218a2678861c637c7
PedroCastro22 <2397775d@student.gla.ac.uk>
2019-01-24 15:47:07 +0000

    fourth commit

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
 	return render(request, 'rango/index.html', context=context_dict)
 	
+
 def about(request):
 	context_dict = {'yourname': "Pedro Castro"}
 	return render(request, 'rango/about.html', context=context_dict)
-	#return HttpResponse("Rango says here is the about page. Click <a href='/rango/'>here</a> to go back to the main page.")
+	
+
+def show_category(request, category_name_slug):
+	context_dict = {}
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	
+	except Category.DoesNotExist:
+		context_dict['pages'] = None
+		context_dict['category'] = None
+	
+	return render(request, 'rango/category.html', context=context_dict)
 	
